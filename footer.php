c4d977a1fce8f401e48da477a5a39a8514493214
achmadsalmanpasha <87318407+achmadsalmanpasha@users.noreply.github.com>
2025-05-26 10:43:52 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+        <div class="container">
+            <p>&copy; <?php echo date('Y'); ?> TUKU. All rights reserved.</p>
+            </div>
+    </footer>
+
+    </body>
+</html>
